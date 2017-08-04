82be3ebf9ecb96a78036c9af67c230ee628a43d4
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-08-04 08:53:09 -0400

    Geolocation and temp working

--- js/weather.js
+++ js/weather.js
@@ -2,20 +2,28 @@
 var lat;
 var lon;
 
+function geoFallback() {
+
+}
 
 // Code to be executed immediately
 (function(){
-  if ("geolocation" in navigator) {
-    /* geolocation is available */
-    navigator.geolocation.getCurrentPosition((position) => {
+  if (navigator.geolocation) {
+    // geolocation IS available
+    navigator.geolocation.getCurrentPosition(function(position) {
       lat = position.coords.latitude;
       lon = position.coords.longitude;
       console.log(lat, lon);
       isItRaining();
+    }, function(error) {
+      // ...but it didn't work...
+      console.log(error);
+      geoFallback();
     });
   } else {
-    /* geolocation IS NOT available */
-    console.log("Executing Plan B.");
+    // geolocation IS NOT available
+    console.log("Looks like geolocation is not available. Executing Plan B...");
+    geoFallback();
   };
 })();
 
@@ -28,15 +36,16 @@ function isItRaining() {
       APPID: "b38e53f0c663c60a0f289cb29826d117"
     }
   })
-  .then(function (response) {
+  .then(function(response) {
     console.log(response);
-    var responseBody = document.createElement('p');
-    responseBody.innerHTML = "It is " + Math.round(response.data.main.temp - 273.15) + " degrees Celcius and " + response.data.weather[0].description + " in " + response.data.name + ".";
-    document.getElementById('results').appendChild(responseBody);
+    document.getElementById('tempC').innerHTML = Math.round(response.data.main.temp - 273.15);
+    // var responseBody = document.createElement('p');
+    // responseBody.innerHTML = "It is " + Math.round(response.data.main.temp - 273.15) + " degrees Celcius and " + response.data.weather[0].description + " in " + response.data.name + ".";
+    // document.getElementById('results').appendChild(responseBody);
   })
-  .catch(function (error) {
+  .catch(function(error) {
     console.log(error);
   });
 }
 
-document.getElementById('button').addEventListener("click", isItRaining);
+// document.getElementById('button').addEventListener("click", isItRaining);
