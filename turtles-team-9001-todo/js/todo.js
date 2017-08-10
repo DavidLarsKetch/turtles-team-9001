29c17282570aa36a39138b9954a81a06085c47d6
David Lars Ketch <davidlarsketch@gmail.com>
2017-08-10 16:47:45 -0500

    Basic Todo item addition
    Finished:
    - Basic addition of todo items (thru button)
    - Checkbox click crosses out todo item
    
    Next todos:
    - Erase text in input field after item is added to the list
    - Todo items are added via 'enter', not button
    - Styling todo list
    - Crossed out todo items are deleted after a certain interval

--- turtles-team-9001-todo/js/todo.js
+++ turtles-team-9001-todo/js/todo.js
@@ -1,30 +1,43 @@
-    function todoAddNew() {
-      var todoNewRawInput = document.getElementById("todoInputField").value;
+function todoAddNew() {
+  var todoRawInput = document.getElementById("todoInputField").value;
 //Need to check whether input field is blank before creating new todo item.
-      if (1===1) {
-        var todoNewTodoItem = document.createElement("li");
-        var todoNewCheckboxWrap = document.createElement("input");
-        todoNewCheckboxWrap.setAttribute("type", "checkbox");
-        todoNewCheckboxWrap.setAttribute("name", "todoItemCheckbox");
-        todoNewCheckboxWrap.setAttribute("id", "todoItemCheckbox");
-        var todoNewTextWrap = document.createElement("p");
+  if (1===1) {
+    var todoItemWrap = document.createElement("li");
 
-        var todoNewTextNode = document.createTextNode(todoNewRawInput);
+    var todoCheckbox = document.createElement("input");
+    todoCheckbox.setAttribute("type", "checkbox");
+    todoCheckbox.setAttribute("name", "todoItemCheckbox");
+    todoCheckbox.setAttribute("id", "todoItemCheckbox");
+    todoCheckbox.setAttribute("style", "display:inline");
+    todoCheckbox.addEventListener("click", todoLineThrough);
 
-        todoNewTextWrap.appendChild(todoNewTextNode);
-        todoNewTodoItem.appendChild(todoNewCheckboxWrap);
-        todoNewTodoItem.appendChild(todoNewTextWrap);
+    var todoTextWrap = document.createElement("p");
+    todoTextWrap.setAttribute("id", "todoItemName");
+    todoTextWrap.setAttribute("style", "display:inline");
 
-        var todoListContainer = document.getElementById("todoList");
+    var todoTextNode = document.createTextNode(todoRawInput);
 
-        todoListContainer.appendChild(todoNewTodoItem);
-      }
-    }
+    todoTextWrap.appendChild(todoTextNode);
+    todoItemWrap.appendChild(todoCheckbox);
+    todoItemWrap.appendChild(todoTextWrap);
 
-    window.onload = function(){
-      document.getElementById("todoInputButton").addEventListener("click", todoAddNew);
-//Need function to change style of checked todo items to crossed out.
+    var todoListContainer = document.getElementById("todoList");
+    todoListContainer.appendChild(todoItemWrap);
+  }
+}
+
+function todoLineThrough() {
+  var x = this.nextElementSibling;
+  x.classList.toggle("todoItemDone");
+}
 
 //Need function to erase crossed out todos after certain interval.
+function todoEraseDoneItems() {
+  console.log("The todoEraseDoneItems function will work!");
+}
 
-    }
+
+window.onload = function(){
+  document.getElementById("todoInputButton").addEventListener("click", todoAddNew);
+  todoEraseDoneItems();
+}
