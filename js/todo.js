892ef1dd3c8cc76f76639994d38d76353a2b3fb4
David Lars Ketch <davidlarsketch@gmail.com>
2017-08-15 20:20:17 -0500

    Built todoDevClearStorage()
    - todoDevClearStorage() is for us to clear out localStorage while developing the todo mod.
    - To be removed at production.

--- js/todo.js
+++ js/todo.js
@@ -79,11 +79,17 @@ function todoStore() {
   window.localStorage.setItem(todoRandomId, todoRawInput);
 }
 
+//Function for clearing localStorage while developing. Erase in production code.
+function todoDevClearStorage () {
+  for (var i = 0; i < todoListKeys.length; i++) {
+    localStorage.removeItem(todoListKeys[i]);
+  };
 }
 
 window.onload = function(){
   todoEraseDoneItems();
+//  todoDevClearStorage(); //Clear double slash to use
   todoRetrieve();
   todoCount();
-  todoSubmitListen();
+  todoListenToSubmit();
 }
