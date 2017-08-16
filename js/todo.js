2e62b69d5976de4948e842eaadbc87e70245a63e
David Lars Ketch <davidlarsketch@gmail.com>
2017-08-16 11:34:35 -0500

    Moving functions around in todo.js
    Stalling again...

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,29 @@
 var todoRawInput, todoRandomId;
 var todoListKeys = Object.keys(localStorage);
 
-function todoMakeRandomId() {
-  todoRandomId = "t" + Date.now();
+//Function to erase crossed out todos after certain interval.
+//All crossed off items should be erased at the same time, e.g., at 00:00.
+//Upon loading extension, before stored todo items are displayed, this function runs, erasing any checked off todo items that have lapsed the time interval.
+function todoEraseDoneItems() {
+  var x = "";
+  console.log("'" + x + "' = output of todoEraseDoneItems function.");
 }
+
+//Function to retrieve & display stored todo items.
+function todoRetrieve() {
+  todoListKeys.sort();
+  for (var i = 0; i < todoListKeys.length; i++) {
+    todoRawInput = localStorage.getItem(todoListKeys[i]);
+    todoConstructItem(todoRawInput);
+  }
+}
+
+//Function to count number of todo items left, not counting those checked off.
+function todoCount() {
+  var todoNumberOfItems = todoListKeys.length;
+  console.log(todoNumberOfItems);
+}
+
 
 function todoListenToSubmit() {
   var todoForm = document.getElementById("todoInputForm");
@@ -14,6 +34,10 @@ function todoListenToSubmit() {
     todoForm.reset();
     event.preventDefault();
   });
+}
+
+function todoMakeRandomId() {
+  todoRandomId = "t" + Date.now();
 }
 
 function todoConstructItem() {
@@ -44,39 +68,16 @@ function todoConstructItem() {
   }
 }
 
+//Function to store todo items.
+function todoStore() {
+  window.localStorage.setItem(todoRandomId, todoRawInput);
+}
+
 //This function should give the stored item a timestamp to be used for erasing items at a certain interval.
 function todoCheckoff() {
   var x = this.nextElementSibling;
   x.classList.toggle("todoItemDone");
   todoCount();
-}
-
-//Function to erase crossed out todos after certain interval.
-//All crossed off items should be erased at the same time, e.g., at 00:00.
-//Upon loading extension, before stored todo items are displayed, this function runs, erasing any checked off todo items that have lapsed the time interval.
-function todoEraseDoneItems() {
-  var x = "";
-  console.log("'" + x + "' = output of todoEraseDoneItems function.");
-}
-
-//Function to retrieve & display stored todo items.
-function todoRetrieve() {
-  todoListKeys.sort();
-  for (var i = 0; i < todoListKeys.length; i++) {
-    todoRawInput = localStorage.getItem(todoListKeys[i]);
-    todoConstructItem(todoRawInput);
-  }
-}
-
-//Function to count number of todo items left, not counting those checked off.
-function todoCount() {
-  var todoNumberOfItems = todoListKeys.length;
-  console.log(todoNumberOfItems);
-}
-
-//Function to store todo items.
-function todoStore() {
-  window.localStorage.setItem(todoRandomId, todoRawInput);
 }
 
 //Function for clearing localStorage while developing. Erase in production code.
