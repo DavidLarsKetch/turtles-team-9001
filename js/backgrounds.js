02d5c436654f7c023071e9963a2e288b87f0697d
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-08-20 14:19:51 -0400

    Background images!

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -1,4 +1,14 @@
 (() => {
-  axios.get('http://www.reddit.com/r/earthporn/.json?show=all&limit=50')
-    .then(function(res){console.log(res.data.data.children)});
+  axios.get('http://www.reddit.com/r/earthporn/.json', {
+    params: {
+      show: 'all',
+      limit: 50
+    }
+  })
+    .then((response) => {
+      var i = Math.floor(Math.random() * 49);
+      var imgURL = response.data.data.children[i].data.url;
+      console.log(response.data.data.children[i].data);
+      document.querySelector('body').setAttribute("style", "background-image: url('" + imgURL + "')");
+    });
 })();
