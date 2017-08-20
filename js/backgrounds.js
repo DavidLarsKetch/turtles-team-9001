f479437f205fa08d66d326f1f7a476c12ae67f43
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-08-20 14:42:12 -0400

    Add credits and make it pretty

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -7,8 +7,10 @@
   })
     .then((response) => {
       var i = Math.floor(Math.random() * 49);
-      var imgURL = response.data.data.children[i].data.url;
+      var img = response.data.data.children[i].data;
       console.log(response.data.data.children[i].data);
-      document.querySelector('body').setAttribute("style", "background-image: url('" + imgURL + "')");
+      document.querySelector('body').setAttribute("style", "background-image: url('" + img.url + "')");
+      document.getElementById('bgTitle').innerHTML = '<a href="http://reddit.com' + img.permalink + '" target="_blank">' + img.title.substring(0, 30).trim() + '...</a>';
+      document.getElementById('bgUser').innerHTML = img.author + ' on <a href="https://www.reddit.com/r/EarthPorn/" target="_blank">r/EarthPorn</a>';
     });
 })();
