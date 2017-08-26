7d10d87f4fc046f6351186d48512ff985422b398
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-08-26 13:53:28 -0400

    Use image preview source url

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -8,8 +8,9 @@
     .then((response) => {
       var i = Math.floor(Math.random() * 49);
       var img = response.data.data.children[i].data;
-      console.log(response.data.data.children[i].data);
-      document.querySelector('body').setAttribute("style", "background-image: url('" + img.url + "')");
+      // console.log(response.data.data.children[i].data);
+      // console.log(response.data.data.children[i].data.preview.images);
+      document.querySelector('body').setAttribute("style", "background-image: url('" + img.preview.images[0].source.url + "')");
       document.getElementById('bgTitle').innerHTML = '<a href="http://reddit.com' + img.permalink + '" target="_blank">' + img.title.substring(0, 30).trim() + '...</a>';
       document.getElementById('bgUser').innerHTML = img.author + ' on <a href="https://www.reddit.com/r/EarthPorn/" target="_blank">r/EarthPorn</a>';
     });
