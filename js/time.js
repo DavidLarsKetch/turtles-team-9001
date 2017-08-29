f437e817a45ffc10868c6f07e9233e30d2bdd80a
[nabernardo] <nabernardo93@gmail.com>
2017-08-29 15:56:27 -0700

    Delete greeting.js and add it to time.js

--- js/time.js
+++ js/time.js
@@ -5,17 +5,39 @@ function setTime() {
     var sec = time.getSeconds();
     var theTimeIs = document.getElementsByClassName("time")[0];
 
-    if (hr < 10) {
-        hr = '0' + hr;
-    }
-    if (min < 10) {
-        min = '0' + min;
-    }
-    if (sec < 10) {
-        sec = '0' + sec;
-    }
+    (function putTime() {
+        if (hr < 10) {
+            hr = '0' + hr;
+        }
+        if (min < 10) {
+            min = '0' + min;
+        }
+        if (sec < 10) {
+            sec = '0' + sec;
+        }
+        
+        theTimeIs.innerHTML = hr + ':'+ min + ':' + sec;
+    })();
+
+    (function greeting() {
+        const greet = document.getElementsByClassName('greeting');
     
-    theTimeIs.innerHTML = hr + ':'+ min + ':' + sec;
+        if (hr > 0 && hr < 12) {
+            greet[0].textContent = 'Good Morning';
+        }
+        
+        if (hr == 12) {
+            greet[0].textContent = 'Good Noon';
+        }
+    
+        if (hr > 12 && hr < 18) {
+            greet[0].textContent = 'Good Afternoon';
+        }
+    
+        if (hr > 18 || hr == 0) {
+            greet[0].textContent = 'Good Evening';
+        }
+    })();
 }
 
 var updatedTime = setInterval(setTime, 500);
